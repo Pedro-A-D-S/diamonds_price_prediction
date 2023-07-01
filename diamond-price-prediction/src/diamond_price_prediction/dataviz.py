fdd7fdf4669786534906bfe693c8849b4187ccae
Pedro Alves <pedroalves0409@gmail.com>
2023-07-01 11:25:41 -0300

    cleaning-files-and-updating-dataviz-script

--- diamond-price-prediction/src/diamond_price_prediction/dataviz.py
+++ diamond-price-prediction/src/diamond_price_prediction/dataviz.py
@@ -1,36 +1,93 @@
 import pandas as pd
 import seaborn as sns
+from typing import Union
+import matplotlib.pyplot as plt
 
-def plot_quantity(data, col):
-    ax = sns.set(rc = {'figure.figsize': (10, 6)})
+
+def plot_quantity(data: pd.DataFrame, col: str) -> plt.Axes:
+    """
+    Plots the quantity of items by a specific category in a bar chart.
+
+    Args:
+        data (pd.DataFrame): The input DataFrame containing the data.
+        col (str): The column name representing the category.
+
+    Returns:
+        plt.Axes: The plot object representing the bar chart.
+    """
+    ax = sns.set(rc={'figure.figsize': (10, 6)})
     order = data[col].value_counts().index
-    ax = sns.countplot(x = col, data = data, order = order)
-    ax.set_title('Quantity of diamonds by {}'.format(col), fontsize = 16)
-    ax.set_xlabel(f'{col}', fontsize = 14)
-    ax.set_ylabel('Quantity', fontsize = 14)
-    ax = ax
+    ax = sns.countplot(x=col, data=data, order=order)
+    ax.set_title(f'Quantity of items by {col}', fontsize=16)
+    ax.set_xlabel(col, fontsize=14)
+    ax.set_ylabel('Quantity', fontsize=14)
     return ax
 
-def mean_price_by_cathegory(data, col):
-    mean_price_by_cathegory = data.groupby([col]).mean().price.round(2).sort_values(ascending = False).reset_index()
-    mean_price_by_cathegory = pd.DataFrame(mean_price_by_cathegory)
-    mean_price_by_cathegory.rename(columns = {'price':'mean price'}, inplace = True)
-    mean_price_by_cathegory
-    
-    return mean_price_by_cathegory
 
-def median_price_by_cathegory(data, col):
-    median_price_by_cathegory = data.groupby([col]).median().price.round(2).sort_values(ascending = False).reset_index()
-    median_price_by_cathegory = pd.DataFrame(median_price_by_cathegory)
-    median_price_by_cathegory.rename(columns = {'price':'median price'}, inplace = True)
-    median_price_by_cathegory
-    
-    return median_price_by_cathegory
+def calculate_price_statistics(data: pd.DataFrame, col: str, statistic: str) -> pd.DataFrame:
+    """
+    Calculates the specified statistic (mean, median, or standard deviation) of prices
+    grouped by a specific category.
 
-def std_price_by_cathegory(data, col):
-    std_price_by_clarity = data.groupby([col]).std().price.round(2).sort_values(ascending = False).reset_index()
-    std_price_by_clarity = pd.DataFrame(std_price_by_clarity)
-    std_price_by_clarity.rename(columns = {'price':'std of price'}, inplace = True)
-    std_price_by_clarity
+    Args:
+        data (pd.DataFrame): The input DataFrame containing the data.
+        col (str): The column name representing the category.
+        statistic (str): The statistic to calculate ('mean', 'median', or 'std').
 
-    return std_price_by_clarity
+    Returns:
+        pd.DataFrame: The DataFrame with the calculated statistic for each category.
+    """
+    if statistic == 'mean':
+        result = data.groupby([col])['price'].mean()
+    elif statistic == 'median':
+        result = data.groupby([col])['price'].median()
+    elif statistic == 'std':
+        result = data.groupby([col])['price'].std()
+    else:
+        raise ValueError("Invalid statistic. Must be one of: 'mean', 'median', 'std'.")
+
+    result = result.round(2).sort_values(ascending=False).reset_index()
+    result.rename(columns={'price': f'{statistic} price'}, inplace=True)
+    return result
+
+
+def mean_price_by_category(data: pd.DataFrame, col: str) -> pd.DataFrame:
+    """
+    Calculates the mean price of items grouped by a specific category.
+
+    Args:
+        data (pd.DataFrame): The input DataFrame containing the data.
+        col (str): The column name representing the category.
+
+    Returns:
+        pd.DataFrame: The DataFrame with the mean price for each category.
+    """
+    return calculate_price_statistics(data, col, 'mean')
+
+
+def median_price_by_category(data: pd.DataFrame, col: str) -> pd.DataFrame:
+    """
+    Calculates the median price of items grouped by a specific category.
+
+    Args:
+        data (pd.DataFrame): The input DataFrame containing the data.
+        col (str): The column name representing the category.
+
+    Returns:
+        pd.DataFrame: The DataFrame with the median price for each category.
+    """
+    return calculate_price_statistics(data, col, 'median')
+
+
+def std_price_by_category(data: pd.DataFrame, col: str) -> pd.DataFrame:
+    """
+    Calculates the standard deviation of prices of items grouped by a specific category.
+
+    Args:
+        data (pd.DataFrame): The input DataFrame containing the data.
+        col (str): The column name representing the category.
+
+    Returns:
+        pd.DataFrame: The DataFrame with the standard deviation of prices for each category.
+    """
+    return calculate_price_statistics(data, col, 'std')
